0387df599c910c072e8737a9cf1cd697ce12e6b5
Zee Chen <zeechen@us.ibm.com>
2016-10-21 17:07:35 -0700

    Updates

--- btrace/SparkCalls.java
+++ btrace/SparkCalls.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the Classpath exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package com.sun.btrace.samples;
 
 import com.sun.btrace.AnyType;
@@ -30,12 +5,60 @@ import com.sun.btrace.annotations.*;
 import static com.sun.btrace.BTraceUtils.*;
 
 @BTrace public class SparkCalls {
-    @OnMethod(clazz="/org\\.apache\\.spark\\..*/", method="/.*/")
-    public static void o(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args) { // all calls to methods
+    /*
+    @OnMethod(
+	      clazz="org.apache.spark.util.ByteBufferInputStream",
+	      method="buffer",
+	      location=@Location(value=Kind.CALL, clazz="/.+/", method="/.+/")
+	      )
+    public static void o(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args) //, @Return AnyType ret)
+    {
+	println("Context: " + pcm + "#" +  pmn);
+	println(self);
+        printArray(args);
+	//println(ret);
+	jstack();
+	println("\n\n");
+    }
+*/
+
+    @OnMethod(
+	      clazz="/org\\.apache\\.spark\\..+/",
+	      method="/.+huffle.*/"
+    )
+    public static void shuffle1(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args)
+    {
 	println("Context: " + pcm + "#" +  pmn);
 	println(self);
         printArray(args);
 	jstack();
 	println("\n\n");
     }
+
+    @OnMethod(
+	      clazz="/org\\.apache\\.spark\\..+huffl.+/",
+	      method="/.+/"
+    )
+    public static void shuffle2(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args)
+    {
+	println("Context: " + pcm + "#" +  pmn);
+	println(self);
+        printArray(args);
+	jstack();
+	println("\n\n");
+    }
+
+    @OnMethod(
+	      clazz="/org\\.apache\\.spark\\.scheduler.+/",
+	      method="/.+/"
+    )
+    public static void sched1(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args)
+    {
+	println("Context: " + pcm + "#" +  pmn);
+	println(self);
+        printArray(args);
+	jstack();
+	println("\n\n");
+    }
+
 } 
