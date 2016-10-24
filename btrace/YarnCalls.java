720569782ea70016c2b80acf8b43edef0b5ce5ab
Zee Chen <zeechen@us.ibm.com>
2016-10-24 15:03:51 -0700

    Add call return probe.

--- btrace/YarnCalls.java
+++ btrace/YarnCalls.java
@@ -7,14 +7,32 @@ import static com.sun.btrace.BTraceUtils.*;
 @BTrace public class YarnCalls {
     @OnMethod(
 	      clazz="/org\\.apache\\.hadoop\\.yarn.+/",
-	      method="/.*/"
+	      method="/.+/"
     )
     public static void yarn1(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, AnyType[] args)
     {
+	Thread t = currentThread();
+	println(t + " id(" + threadId(t) + ")");
 	println("Context: " + pcm + "#" +  pmn);
-	println(self);
+	println("This: " + self);
         printArray(args);
 	jstack();
+	println("\n\n");
+    }
+
+    @OnMethod(
+	      clazz="/org\\.apache\\.hadoop\\.yarn.+/",
+	      method="/.+/",
+	      location=@Location(Kind.RETURN)
+    )
+    public static void yarn2(@Self Object self, @ProbeClassName String pcm, @ProbeMethodName String pmn, @Return AnyType ret)
+    {
+	Thread t = currentThread();
+	println(t + " id(" + threadId(t) + ")");
+	println("Context: " + pcm + "#" +  pmn);
+	println("This: " + self);
+        //printArray(args);
+	println("Return: " + ret);
 	println("\n\n");
     }
 
